894322ab075992478255a80e73c4f58d06b84421
Kirill <kiribon4ik@gmail.com>
2020-07-07 11:52:03 +0500

    Initial commit lesson 10

---
+++ Lesson_10/Task_1.sql
@@ -0,0 +1,25 @@
+-- 1. Проанализировать какие запросы могут выполняться наиболее
+-- часто в процессе работы приложения и добавить необходимые индексы.
+
+USE vk;
+
+CREATE UNIQUE INDEX user_email_uq ON users(email);
+
+CREATE UNIQUE INDEX user_phone_uq ON users(phone);
+
+CREATE INDEX users_first_name_idx ON users(first_name);
+
+CREATE INDEX users_last_name_idx ON users(last_name);
+
+CREATE INDEX profiles_photo_id_birthday_city_country_idx ON profiles(photo_id, birthday, city, country);
+
+CREATE INDEX messages_from_user_id_to_user_id_idx ON messages(from_user_id, to_user_id);
+
+CREATE INDEX media_user_id_filename_idx ON media(user_id, filename);
+
+CREATE INDEX likes_target_type_id_idx ON likes(target_type_id);
+
+CREATE INDEX friendship_friend_id_idx ON friendship(friend_id);
+
+CREATE INDEX communities_name_idx ON communities(name);
+
---
+++ Lesson_10/Task_2.sql
@@ -0,0 +1,34 @@
+-- 2. Задание на оконные функции
+
+-- Построить запрос, который будет выводить следующие столбцы:
+-- имя группы
+-- среднее количество пользователей в группах
+-- самый молодой пользователь в группе
+-- самый старший пользователь в группе
+-- общее количество пользователей в группе
+-- всего пользователей в системе
+-- отношение в процентах (общее количество пользователей в группе / всего пользователей в системе) * 100
+
+USE vk; 
+
+
+SELECT DISTINCT 
+  communities.id AS group_id,
+  communities.name AS name_group, 
+  COUNT(communities_users.user_id) OVER() AS total_users,
+  COUNT(communities_users.user_id) OVER() / (SELECT MAX(communities.id) FROM communities) AS avarage,
+  COUNT(*) OVER(PARTITION BY communities_users.community_id) AS users_in_group,
+  COUNT(*) OVER(PARTITION BY communities_users.community_id) / COUNT(communities_users.user_id) OVER() * 100 AS '%%',
+  MIN(profiles.birthday) OVER(PARTITION BY communities_users.community_id) AS oldest_in_group,   -- не догадался как вывести ID пользователя с этим значением
+  MAX(profiles.birthday) OVER(PARTITION BY communities_users.community_id) AS youngest_in_group  -- не догадался как вывести ID пользователя с этим значением
+FROM communities_users
+  JOIN communities
+    ON communities_users.community_id = communities.id
+  JOIN profiles
+    ON communities_users.user_id = profiles.user_id
+ORDER BY group_id;
+
+
+SELECT profiles.user_id FROM profiles WHERE birthday = (SELECT MIN(profiles.birthday) FROM profiles);
+SELECT profiles.user_id FROM profiles WHERE birthday = (SELECT MAX(profiles.birthday) FROM profiles);
+
